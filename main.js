f9738e9380b0d98e91bef0a43cb9fecb0afab5a5
GrayedFox <che.fisher@gmail.com>
2018-03-11 12:41:56 +1100

    clean up main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const openCsvWriteStream = () => {
   csvWriteStream = fs.createWriteStream('./exportedData.csv', { flags: 'a'})
 }
 
-const promiseScrape = () => {
+const scrapeNextBlock = () => {
   return new Promise( (resolve, reject) => {
     try {
       resolve(scraper(blockHeight, csvWriteStream))
@@ -27,41 +27,28 @@ const promiseScrape = () => {
   })
 }
 
-const worker = async (process) => {
-  let message = await promiseScrape()
-  console.log(`Message from scraper: ${message}`)
-  process.send(message)
-}
-
 const main = () => {
   if (blockEnd === undefined || typeof(blockEnd) !== 'number') {
     console.error('Error! BLOCKSCRAPEEND must be defined in your local environment!')
     process.exit(1)
   }
 
+  const messageHandler = async (msg) => {
+    console.log(`Message handler recieved: ${msg}`)
+    if (!msg || msg === '') {
+      console.error('Error! Message must be defined and cannot be empty string!')
+      return
+    }
+
+    if (msg === 'beginScraping' || msg === 'blockDone') {
+      let result = await scrapeNextBlock()
+      console.log(`Message from scraper: ${result}`)
+      process.send(result) // < -- should send 'blockDone, which in turn calls this message handler recursively'
+    }
+  }
+
   if (cluster.isMaster) {
     console.log(`Master process ${process.pid} is running`)
-    const messageHandler = (msg) => {
-      if (!msg || msg === '') {
-        console.error('Error! Message must be defined and cannot be empty string!')
-        return
-      }
-
-      switch (msg) {
-        case 'nextBlock':
-          console.log('Proceeding to next block')
-          worker(process)
-          break
-
-        case 'shutdown':
-          console.log('Shutting down worker')
-          process.kill()
-          break
-
-        default:
-          console.error(`Should not see this message: ${msg}`)
-      }
-    }
 
     for (let i = 0; i < cores.length; i++) {
       cluster.fork()
@@ -72,12 +59,7 @@ const main = () => {
     }
 
     cluster.on('message', (worker, message) => {
-      console.log(`Worker ${worker.process.pid} sent message: ${message}`)
-      if (blockHeight <= blockEnd) {
-        worker.send('nextBlock') // <-- doesn't work?
-      } else {
-        worker.send('shutdown')
-      }
+      console.log(`Master listened to ${worker.process.pid} send message: ${message}`)
     })
 
     cluster.on('exit', (worker, code, signal) => {
@@ -85,7 +67,7 @@ const main = () => {
     })
   } else {
     console.log(`Worker ${process.pid} started...`)
-    process.send('nextBlock')
+    process.send('beginScraping')
   }
 }
 
