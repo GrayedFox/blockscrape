27aca210b740871f767a9d59491a0161fb333665
GrayedFox <che.fisher@gmail.com>
2018-03-11 16:36:56 +1100

    message passing working! need to fix bug with all workers doing same block

--- main.js
+++ main.js
@@ -33,37 +33,28 @@ const main = () => {
     process.exit(1)
   }
 
-  const messageHandler = async (msg) => {
-    console.log(`Message handler recieved: ${msg}`)
-    if (!msg || msg === '') {
-      console.error('Error! Message must be defined and cannot be empty string!')
-      return
-    }
-
-    if (msg === 'beginScraping' || msg === 'blockDone') {
-      if (blockHeight <= blockEnd) {
-        let result = await scrapeNextBlock()
-        console.log(`Message from scraper: ${result}`)
-        process.send(result) // < -- should send 'blockDone, which in turn calls this message handler recursively'
-      } else {
-        console.log('No more block!')
-        process.kill()
-      }
-    }
-  }
-
   if (cluster.isMaster) {
     console.log(`Master process ${process.pid} is running`)
     for (let i = 0; i < cores.length; i++) {
       cluster.fork()
     }
 
-    for (const id in cluster.workers) {
-      cluster.workers[id].on('message', messageHandler)
-    }
+    // for (const id in cluster.workers) {
+    //   cluster.workers[id].on('message', messageHandler)
+    // }
 
     cluster.on('message', (worker, message) => {
       console.log(`Master listened to ${worker.process.pid} send message: ${message}`)
+      if (blockHeight <= blockEnd) {
+        if (message === 'beginScraping' || message === 'blockDone') {
+          worker.send('nextBlock')
+        } else {
+          console.error(`Invalid message: ${message}, shutting worker down...`)
+          worker.kill(1)
+        }
+      } else {
+        worker.kill()
+      }
     })
 
     cluster.on('exit', (worker, code, signal) => {
@@ -71,6 +62,21 @@ const main = () => {
     })
   } else {
     console.log(`Worker ${process.pid} started...`)
+
+    process.on('message', async (msg) => {
+      console.log(`Message handler recieved: ${msg}`)
+      if (!msg || msg === '') {
+        console.error('Error! Message must be defined and cannot be empty string!')
+        return
+      }
+
+      if (msg === 'nextBlock') {
+        let result = await scrapeNextBlock()
+        console.log(`Message from scraper: ${result}`)
+        process.send(result) // should send 'blockDone to master, which sends 'nextBlock' back if blocks remain
+      }
+    })
+
     process.send('beginScraping')
   }
 }
