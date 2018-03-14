f2bf2a3aaaad18951960f03dec0e6b66c892d0d7
GrayedFox <che.fisher@gmail.com>
2018-03-14 20:23:02 +1100

    tidy up keeper file

--- keeper.js
+++ keeper.js
@@ -1,13 +1,10 @@
 let transactions = {}
 
-const storeTransaction = (tx, vout) => {
-  transactions[tx] = vout
-  console.log(transactions[tx])
-}
+const storeTransaction = (tx, vout) => { transactions[tx] = vout }
 
 const lookupTransaction = (tx) => transactions[tx] ? true : false
 
-const getTransacton = (tx) => transactions[tx].vout
+const getTransacton = (tx) => transactions[tx]
 
 module.exports = {
   storeTransaction,
