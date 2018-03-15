dfd9ec04eeb2266688a0afde10366c805ddb2597
GrayedFox <che.fisher@gmail.com>
2018-03-15 14:49:22 +1100

    add logic to clear retrieved transactions to prevent memory growth

--- globals.js
+++ globals.js
@@ -6,7 +6,9 @@ const storeTransaction = (tx, vout) => { transactions[tx] = vout }
 
 const transactionExists = (tx) => transactions[tx] ? true : false
 
-const retrieveTransacton = (tx) => transactions[tx]
+const retrieveTransaction = (tx) => transactions[tx]
+
+const clearTransaction = (tx) => { delete transactions[tx] }
 
 // loop through the outputs of a tx, greedily returning the value of an output tx where n matches vOutIdx
 const getMatchingTransactionValue = async (txHash, voutIndex) => {
@@ -14,7 +16,8 @@ const getMatchingTransactionValue = async (txHash, voutIndex) => {
   let voutArray = undefined
 
   if (transactionExists(txHash)) {
-    voutArray = retrieveTransacton(txHash)
+    voutArray = retrieveTransaction(txHash)
+    clearTransaction(txHash)
   } else {
     tx = await api.getRawTransaction(txHash)
     voutArray = JSON.parse(tx).vout
