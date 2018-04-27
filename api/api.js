829955fe5abd4379fc785ba64feefbed7dec2385
GrayedFox <che.fisher@gmail.com>
2018-04-27 14:29:03 +0200

    add api documentation and refactor variable names for clarity

--- api/api.js
+++ api/api.js
@@ -2,40 +2,52 @@ const { client, blockchainCli, blockchainApi } = require('../client.js')
 const litecoin = require('./litecoin.js')
 const blockcypher = require('./blockcypher.js')
 
-let blockchain = undefined
+let api = undefined
 
 if (blockchainCli.endsWith('litecoin-cli') || blockchainCli.endsWith('bitcoin-cli')) {
-  blockchain = litecoin
+  api = litecoin
 }
 
 // if both api and cli are defined api is given preference
 if (blockchainApi === 'blockcypher') {
-  blockchain = blockcypher
+  api = blockcypher
 }
 
-const decodeRawTransaction = (txHash) => {
-  return client([blockchain.decodeRawTransaction, txHash])
+/**
+ *  This API acts as a wrapper for whatever local or remote endpoint you're querying. In order to extend it you will
+ *  need to map the matching operation from the local/remote endpoint to one of the below functions. Check the
+ *  blockcypher or litecoin files for examples of mapping commands to this format and make sure to extend the
+ *  IF conditions above for catching and setting the correct API.
+ *
+ *  [shared format for all API commands]
+ *  @param  {[type]} required :: normally a string (valid tx/block hash) or number; this argument is always required
+ *  @param  {[array]} options :: optional params for the command, must be an array even if passing a single value
+ *  @return {[promise]}       :: returns a promise; resolved with result of command, rejected by any error
+ */
+
+const decodeRawTransaction = (txHash, options) => {
+  return client([api.decodeRawTransaction, txHash], options)
 }
 
-const getBlock = (blockhash) => {
-  return client([blockchain.getBlock, blockhash])
+const getBlockByHash = (blockhash, options) => {
+  return client([api.getBlockByHash, blockhash], options)
 }
 
-const getBlockHashByHeight = (height) => {
-  return client([blockchain.getBlockHash, height])
+const getBlockHashByHeight = (height, options) => {
+  return client([api.getBlockHash, height], options)
 }
 
-const getInfo = () => {
-  return client([blockchain.getInfo])
+const getInfo = (options) => {
+  return client([api.getInfo], options)
 }
 
-const getRawTransaction = (txHash, verbose = true) => {
-  return client([blockchain.getRawTransaction, txHash, verbose])
+const getRawTransaction = (txHash, options) => {
+  return client([api.getRawTransaction, txHash], options)
 }
 
 module.exports = {
   decodeRawTransaction,
-  getBlock,
+  getBlockByHash,
   getBlockHashByHeight,
   getInfo,
   getRawTransaction
